7ef601a182bf497e7e44223f646d11e99a69c5b9
Darien Morrison <darien.morrison@jbhunt.com>
2021-08-28 15:05:49 -0500

    Added jackson dataformat xml dependency and corresponding GET mapping variables to support returning either JSON or XML

--- src/main/java/com/udemy_springboot_microservices/mobileappws/ui/controller/UserController.java
+++ src/main/java/com/udemy_springboot_microservices/mobileappws/ui/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.udemy_springboot_microservices.mobileappws.ui.controller;
 
 import com.udemy_springboot_microservices.mobileappws.ui.model.response.User;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -8,20 +9,19 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
   //http:localhost:8080/users/{userID}
-  @GetMapping(path = "/{userID}")
-  public String getUser(@PathVariable String userID) {
-
-    return "get user called with userID = " + userID;
+  @GetMapping(path = "/{userID}", produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+  public User getUser(@PathVariable String userID) {
+    return new User("Quinn", "Morrison", "dmorr041@fiu.edu");
   }
 
   // http://localhost:8080/users?page={page}&limit={limit}&sort={sort}
   @GetMapping
-  public User getUsers(
+  public String getUsers(
     @RequestParam(value = "page", defaultValue = "1") int page,
     @RequestParam(value = "limit", defaultValue = "50") int limit,
     @RequestParam(value = "sort", defaultValue = "desc", required = false) String sort
   ) {
-    return new User("Quinn", "Morrison", "dmorr041@fiu.edu");
+    return "get users called with page = " + page + " and limit = " + limit + " and sorting method = " + sort;
   }
 
   @PostMapping
