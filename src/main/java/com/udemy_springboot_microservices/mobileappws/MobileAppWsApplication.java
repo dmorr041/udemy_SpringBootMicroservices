5c895d3735ae2040e138caaf1192627c23eff590
Darien Morrison <darien.morrison@jbhunt.com>
2021-08-28 12:54:32 -0500

    Added a UserController class with some simple CRUD methods

--- src/main/java/com/udemy_springboot_microservices/mobileappws/MobileAppWsApplication.java
+++ src/main/java/com/udemy_springboot_microservices/mobileappws/MobileAppWsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MobileAppWsApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MobileAppWsApplication.class, args);
 	}
 
